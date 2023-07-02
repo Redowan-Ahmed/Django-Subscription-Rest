dcc06808d1f587504d2bd7a99f16d0a65730804d
Redowan Ahmed <55311231+Redowan-Ahmed@users.noreply.github.com>
2023-07-03 01:18:09 +0600

    First Commit

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -118,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS =[
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = 'uploads/'
+
+MEDIA_ROOT = BASE_DIR / 'uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
